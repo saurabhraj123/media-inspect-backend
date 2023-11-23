5450ba1ff4ca768ec2d84ea0b6649e1ce0c0b9b8
saurabhraj123 <me4saurabh4u@gmail.com>
2023-11-23 17:55:46 +0530

    feat: setup auth route

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import authRouter from "./auth";
 const router = express.Router();
 
-router.use("/auth", authRouter);
+router.get("/signup", (req, res) => {
+  res.send("signup");
+});
 
 export default router;
